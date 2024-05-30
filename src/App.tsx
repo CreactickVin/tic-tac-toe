ef34da240c693d9f7a5fc582734eadb3114b550f
CreactickVin <91427591+CreactickVin@users.noreply.github.com>
2024-05-30 18:44:32 +0530

    --added routing,components,pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import './App.css'
+import {BrowserRouter} from "react-router-dom";
+import {ReactNode} from "react";
+
+function Routes(props: { children: ReactNode }) {
+  return null;
+}
+
+function Route(props: { index: boolean, element: JSX.Element }) {
+  return null;
+}
 
 function App() {
   return (
     <>
-      <div className='bg-red-900'>
-        tjhis
-      </div>
+      <BrowserRouter>
+        <Routes>
+            <Route path="/" index element={<Home />} />
+            <Route path="game" element={<Game />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
