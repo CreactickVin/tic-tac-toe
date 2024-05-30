9d36770ebf110b6dacf701442da78df640b3d665
CreactickVin <91427591+CreactickVin@users.noreply.github.com>
2024-05-30 19:13:31 +0530

    -- added google fonts

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,19 +1,16 @@
+import logo from '../assets/logo.svg'
+
 function Game() {
     return (
-        <div className='bg-[#1a2a33] text-white min-h-screen flex flex-col'>Game
-            <div >
-                <div className='h-6'>
-                    <svg xmlns="http://www.w3.org/2000/svg" id="x">
-                        <path
-                            d="M1.41 0 0 1.41l.72.72L2.5 3.94.72 5.72 0 6.41l1.41 1.44.72-.72 1.81-1.81 1.78 1.81.69.72 1.44-1.44-.72-.69-1.81-1.78 1.81-1.81.72-.72L6.41 0l-.69.72L3.94 2.5 2.13.72 1.41 0z"></path>
-                    </svg>
+        <div className='bg-[#1a2a33] text-white min-h-screen flex flex-col'>
+            <div>
+                <div>
+                    <img src={logo} alt="logo"/>
                 </div>
+
                 <div className='h-6'>
-                    <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 512 512" viewBox="0 0 512 512"
-                         id="o">
-                        <path
-                            d="M256,1C115.167,1,1,115.167,1,256s114.167,255,255,255s255-114.167,255-255S396.833,1,256,1z M256,438.143	c-100.595,0-182.143-81.548-182.143-182.143S155.405,73.857,256,73.857S438.143,155.405,438.143,256S356.595,438.143,256,438.143z"></path>
-                    </svg>
+
+                    TURN
                 </div>
 
                 <button className='bg-[#1f3641] p-4 shadow-[inset 0_-0.4rem_0_#0f191e]'>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        oswald: 'Oswald, ui-serif', // Adds a new `font-oswald` class
+      }
+    },
   },
   plugins: [],
 }
