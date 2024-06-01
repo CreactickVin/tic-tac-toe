e78fe96bf1d85d85c2fef7fd7e2e685ea766cc52
rasesh <devsbtech@gmail.com>
2024-06-01 16:28:29 +0530

    -- home page initial view done

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,31 +1,46 @@
+import xIcon from "../Assets/icon-x.svg";
+import oIcon from "../Assets/icon-o.svg";
+import oDarkIcon from "../Assets/icon-o-dark.svg";
+import oLightIcon from "../Assets/icon-o-light.svg";
+import xDarkIcon from "../Assets/icon-x-dark.svg";
+import xLightIcon from "../Assets/icon-x-light.svg";
+import { useState } from "react";
+
 function Home() {
+  const [isXClicked, setIsXClicked] = useState(true);
+
   return (
-    <div className="bg-[#1a2a33] h-screen flex-col justify-center items-center p-32">
-      <div className="flex justify-center items-center">
-        <svg width="64" height="64" xmlns="http://www.w3.org/2000/svg">
-          <path
-            d="M15.002 1.147 32 18.145 48.998 1.147a3 3 0 0 1 4.243 0l9.612 9.612a3 3 0 0 1 0 4.243L45.855 32l16.998 16.998a3 3 0 0 1 0 4.243l-9.612 9.612a3 3 0 0 1-4.243 0L32 45.855 15.002 62.853a3 3 0 0 1-4.243 0L1.147 53.24a3 3 0 0 1 0-4.243L18.145 32 1.147 15.002a3 3 0 0 1 0-4.243l9.612-9.612a3 3 0 0 1 4.243 0Z"
-            fill="#31C3BD"
-            fill-rule="evenodd"
-          />
-        </svg>
-        <svg width="64" height="64" xmlns="http://www.w3.org/2000/svg">
-          <path
-            d="M32 0c17.673 0 32 14.327 32 32 0 17.673-14.327 32-32 32C14.327 64 0 49.673 0 32 0 14.327 14.327 0 32 0Zm0 18.963c-7.2 0-13.037 5.837-13.037 13.037 0 7.2 5.837 13.037 13.037 13.037 7.2 0 13.037-5.837 13.037-13.037 0-7.2-5.837-13.037-13.037-13.037Z"
-            fill="#F2B137"
-          />
-        </svg>
+    <div className="bg-[#1a2a33] h-screen flex flex-col justify-center items-center">
+      <div className="flex justify-center items-center mb-10 w-1/2">
+        <img src={xIcon} alt="ox-Icon" className="mr-3" width={40} />
+        <img src={oIcon} alt="o-Icon" width={40} />
       </div>
-      <div className="bg-[#1f3641] w-1/3">
-        <h1 className="text-[#a8bfc9] font-outfit font-bold">
+      <div className="bg-[#1f3641] w-1/3 rounded-xl p-6 mb-8 custom-box-shadow-dark">
+        <h1 className="text-[#a8bfc9] font-outfit font-bold mb-4 text-center">
           PICK PLAYER 1'S MARK
         </h1>
-        <button className=""></button>
-        <button className=""></button>
-        <h1 className="text-[#a8bfc9] font-outfit">REMEMBER: X GOES FIRST</h1>
+        <div className="w-full flex justify-around items-center bg-[#1a2a33] p-3 rounded-xl mb-4">
+          <button
+            className={`w-full flex justify-center rounded-xl py-3 transition-colors duration-300 ${isXClicked ? 'bg-[#a8bfc9]' : 'hover:bg-[#1d303a]'}`}
+            onClick={() => setIsXClicked(true)}
+          >
+            <img src={isXClicked ? xDarkIcon : xLightIcon} alt="x-Icon" width={40} />
+          </button>
+          <button
+            className={`w-full flex justify-center rounded-xl py-3 transition-colors duration-300 ${!isXClicked ? 'bg-[#a8bfc9]' : 'hover:bg-[#1d303a]'}`}
+            onClick={() => setIsXClicked(false)}
+          >
+            <img src={!isXClicked ? oDarkIcon : oLightIcon} alt="o-Icon" width={40} />
+          </button>
+        </div>
+        <h1 className="text-[#a8bfc9] font-outfit opacity-40 text-sm text-center">
+          REMEMBER: X GOES FIRST
+        </h1>
       </div>
       <div>
-        <button className="">NEW GAME (VS PLAYER)</button>
+        <button className="px-36 custom-box-shadow-blue text-[#1a2a33] font-outfit p-6 bg-[#31c3bd] font-semibold rounded-xl text-xl">
+          NEW GAME (VS PLAYER)
+        </button>
       </div>
     </div>
   );
