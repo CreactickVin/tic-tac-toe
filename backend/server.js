e5d66ddf78df45f5cf9eef5569f67bac903b2d41
rasesh <raseshradadiya1@gmail.com>
2024-06-03 16:19:46 +0530

    -- backend init

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,21 @@
-console.log("running flip flop");
+const io = require("socket.io")(8080,{
+    cors:{
+        origin : ["http://localhost:5173"],
+    },
+})
+
+io.on("connection", socket => {
+    console.log("connected with id " + socket.id);
+
+    socket.on("move",(gridArray) => {
+        socket.broadcast.emit(gridArray)
+    })
+
+    socket.on("result",(result) => {
+        socket.broadcast.emit(result)
+    })
+
+    socket.on("disconnect",() => {
+        console.log("disconnected with id " + socket.id);
+    })
+})
